3c37c5bd88cc164fa6dd9586d71ce3e5835c884f
Mark Ryan <markrryan99@gmail.com>
2023-12-10 17:34:42 -0800

    first commit

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include "raylib.h"
+
+int main() {
+    float root_beer = 1.99;
+    double cheeseburger {5.99};
+    bool shouldHaveLunch{};
+
+    int width = 350;
+    InitWindow(width, 200, "Mark's Window");
+}
