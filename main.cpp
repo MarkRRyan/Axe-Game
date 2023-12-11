bb3fd8bca5e255ab00640ec31fc87fb1dcd77ef0
Mark Ryan <markrryan99@gmail.com>
2023-12-10 19:01:26 -0800

    adding axe game logic"

--- main.cpp
+++ main.cpp
@@ -1,10 +1,40 @@
 #include "raylib.h"
 
 int main() {
-    float root_beer = 1.99;
-    double cheeseburger {5.99};
-    bool shouldHaveLunch{};
 
+    // window dimensions
     int width = 350;
-    InitWindow(width, 200, "Mark's Window");
+    int height = 200;
+    InitWindow(width, height, "Game Window");
+
+    // circle coordinates
+    int circle_x{175};
+    int circle_y{100};
+
+    SetTargetFPS(60);
+
+    while ( WindowShouldClose() == false ) {
+        BeginDrawing();
+        ClearBackground(WHITE);
+
+        // Game logic begins
+
+        DrawCircle( circle_x, circle_y, 25, BLUE);
+
+        if ((IsKeyDown(KEY_D) || IsKeyDown(KEY_RIGHT)) && circle_x < 350 ) {
+            circle_x += 10;
+        }
+        if ((IsKeyDown(KEY_A) || IsKeyDown(KEY_LEFT)) && circle_x > 0 ) {
+            circle_x -= 10;
+        }
+        // if (IsKeyDown(KEY_W)) {
+        //     circle_y -= 10;
+        // }
+        // if (IsKeyDown(KEY_S)) {
+        //     circle_y += 10;
+        // }
+
+        // Game logic ends
+        EndDrawing();
+    }
 }
